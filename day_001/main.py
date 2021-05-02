03a0da5052befbfa0a2dd0ed81dd88af3da639fa
Marco Fuchs <marco.fuchs@live.de>
2021-05-02 16:14:09 +0200

    Fix document style with black

--- day_001/main.py
+++ day_001/main.py
@@ -12,12 +12,11 @@ print('what to print')
 )
 
 # Exercise 1-2 - Debugging
-#Fix the code below 👇
+# Fix the code below 👇
 # print(Day 1 - String Manipulation")
 # print("String Concatenation is done with the "+" sign.")
 #   print('e.g. print("Hello " + "world")')
 # print(("New lines can be created with a backslash and n.")
-
 print("Day 1 - String Manipulation")
 print("String Concatenation is done with the " + " sign.")
 print('e.g. print("Hello " + "world")')
