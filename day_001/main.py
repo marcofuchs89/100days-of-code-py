3a5cab6cf89c1bbb283e814d82d9179acdda35a6
Marco Fuchs <marco.fuchs@live.de>
2021-05-02 15:33:54 +0200

    Add main.py and exercise-1-1.md

---
+++ day_001/main.py
@@ -0,0 +1,12 @@
+# First Lesson - print() statements
+print("100 Days of Python - Day 1")
+print("==========================")
+print("")
+
+# Exercise 1-1
+print(
+    """Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')
+"""
+)
