3f12931fb253308db42fc7dab11a66f105bf9352
Marco Fuchs <marco.fuchs@live.de>
2021-05-02 15:52:21 +0200

    Complete Coding Exercise 1-2 on debugging

--- day_001/main.py
+++ day_001/main.py
@@ -3,10 +3,22 @@ print("100 Days of Python - Day 1")
 print("==========================")
 print("")
 
-# Exercise 1-1
+# Exercise 1-1 - Printing
 print(
     """Day 1 - Python Print Function
 The function is declared like this:
 print('what to print')
 """
 )
+
+# Exercise 1-2 - Debugging
+#Fix the code below 👇
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#   print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the " + " sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
