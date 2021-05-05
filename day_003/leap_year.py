3b97cc021ef224813abe01300e9c237d178ec989
Marco Fuchs <marco.fuchs@live.de>
2021-05-05 08:36:33 +0200

    Fix typo in response.

--- day_003/leap_year.py
+++ day_003/leap_year.py
@@ -5,7 +5,7 @@ year = int(input("Which year do you want to check? "))
 if year % 4 == 0:
     if year % 100 == 0:
         if year % 400 == 0:
-            print("Leap your.")
+            print("Leap year.")
         else:
             print("Not leap year.")
     else:
