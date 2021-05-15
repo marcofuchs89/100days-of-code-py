ec15a0906c320de2983f37fa1bdc43a573a81287
Marco Fuchs <marco.fuchs@live.de>
2021-05-15 17:16:30 +0200

    Changed len() to a counter in the for loop

--- day_005/avg_height.py
+++ day_005/avg_height.py
@@ -5,11 +5,15 @@ for n in range(0, len(student_heights)):
 # 🚨 Don't change the code above 👆
 
 # Write your code below this row 👇
+# Remark: len() and sum() are forbidden to use!
 
 average_height: int = 0
+item_count: int = 0
 
+# Sum up the heights and counting the iterations
 for height in student_heights:
     average_height += height
+    item_count += 1
 
-average_height: int = round(average_height / len(student_heights))
+average_height: int = round(average_height / item_count)
 print(average_height)
