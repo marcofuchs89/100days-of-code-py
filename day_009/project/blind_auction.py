914c7c185eb968db6d44657f0916588358532b33
Marco Fuchs <marco.fuchs@live.de>
2021-05-25 18:04:02 +0200

    Remove unused imports; define a function to find the winner; restructured code

--- day_009/project/blind_auction.py
+++ day_009/project/blind_auction.py
@@ -1,34 +1,36 @@
 # Blind/Sealed Auction
-from typing import Dict
 from art import logo
 from os import system
 
 # initialize needed helper variables
-winner = ""
-highest_bid = 0
-new_auctioneer = True
+bidding_finished = False
 bid_register = {}
+
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bid_register:
+        if bid_register[bidder] > highest_bid:
+            highest_bid = bid_register[bidder]
+            winner = bidder
+
+    print(f"{winner} has won the auction with a bid of ${highest_bid}")
+
 
 if __name__ == "__main__":
     print(logo)
     print("Welcome to the sealed auction!")
 
-    while new_auctioneer:
-        user_name = input("What's your name?: ")
-        bid_amount = int(input("What's your bid price?: $"))
+    while not bidding_finished:
+        user_name = input("What is your name?: ")
+        bid_amount = int(input("What is your bid?: $"))
         bid_register[user_name] = bid_amount
         another_auctioneer = input(
-            "Is there another person who want's to bid? (yes/no): "
-        )
+            "Are there any other bidders? Type 'yes or 'no'.\n"
+        ).lower()
         if another_auctioneer == "no":
-            new_auctioneer = False
-        system("clear")
-
-    for user in bid_register:
-        if bid_register[user] > highest_bid:
-            highest_bid = bid_register[user]
-            winner = user
-        else:
-            pass
-
-    print(f"{winner} has won the auction with a bid of ${highest_bid}")
+            bidding_finished = True
+            find_highest_bidder(bid_register)
+        elif another_auctioneer == "yes":
+            system("clear")
