e068ffb285d4c09d3faa16ee4b58dcf74cc52657
Marco Fuchs <marco.fuchs@live.de>
2021-05-25 19:13:43 +0200

    Reformatting the sample code

--- day_010/sample_code.py
+++ day_010/sample_code.py
@@ -1,1 +1,17 @@
 # Init day 10
+
+# Basic functions with outputs
+
+def format_name(f_name: str, l_name: str) -> str:
+    """
+    Takes two strings as an input and joins them together
+    with a space seperator.
+    It then returns the string in Titlecase.
+    \n
+    Example:\n
+    format_name("max", "MUsteR") returns -> "Max Muster"
+    """
+    return ' '.join([f_name, l_name]).title()
+
+
+print(format_name("mArCo", "FuCHs"))
