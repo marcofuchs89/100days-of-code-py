5edc88dfd6c94e4852a062aba6b3facc800ff5f9
Marco Fuchs <marco.fuchs@live.de>
2021-05-26 15:47:12 +0200

    Add continuation of the calculation with the already calculated result

--- day_010/project/calculator.py
+++ day_010/project/calculator.py
@@ -39,15 +39,20 @@ operations: Dict[str, Callable] = {
 
 # Program Start
 print(logo)
+finished_calculation = False
 
 num1 = int(input("What's the first number?: "))
 
 for symbol in operations:
     print(symbol)
-operation_symbol = (input("Pick an operation from the lines above: "))
 
-num2 = int(input("What's the second number?: "))
-
-answer = operations[operation_symbol](num1, num2)
-
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+while not finished_calculation:
+    operation_symbol = input("Pick an operation: ")
+    num2 = int(input("What's the next number?: "))
+    answer = operations[operation_symbol](num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+    another_calculation = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit: ").lower()
+    if another_calculation == 'y':
+        num1 = answer
+    else:
+        finished_calculation = True
